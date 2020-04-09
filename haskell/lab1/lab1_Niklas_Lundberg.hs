f34697843e10a24c2e8b087965540e9c7d06aa9d
Blinningjr <nicke.l@telia.com>
2020-04-09 20:12:07 +0200

    Added some useful functions for lab 1

--- haskell/lab1/lab1_Niklas_Lundberg.hs
+++ haskell/lab1/lab1_Niklas_Lundberg.hs
@@ -1,5 +1,17 @@
-allSubArr :: [Int] -> [[Int]]
-allSubArr (x:xs)
- where 
+rmLast :: [Int] -> [Int]
+rmLast (x:xs)
+ | length (x:xs) < 2 = []
+ | otherwise = x : rmLast xs
 
+sumArr :: [int] -> int
+sumArr [] = 0
+sumArr (x:xs) = x + sumArr xs
+
+allSubArr :: [Int] -> [[Int]]
+allSubArr [] = []
+allSubArr (x:xs) = hallSubArr (x:xs) ++ allSubArr xs
+ where 
+  hallSubArr :: [Int] -> [[Int]]
+  hallSubArr [] = []
+  hallSubArr xs = xs : hallSubArr (rmLast xs)
 
