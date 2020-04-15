c9e083b40e42e347da7a9c9d81a8f11268b17931
Blinningjr <nicke.l@telia.com>
2020-04-15 12:19:43 +0200

    Did more exercises for lecture 3

--- haskell/exercises/exercises3.hs
+++ haskell/exercises/exercises3.hs
@@ -62,13 +62,61 @@ fEqual :: (Int -> Int) -> Int -> Bool
 fEqual f n = allEq (map f [0..n]) 
 
 fGreater :: (Int -> Int) -> Int -> Bool
-fGreater f n = 
+fGreater f n = res 
+ where
+  greater :: [Int] -> (Int, Bool)
+  greater [] = (0, True)
+  greater (x:xs) = (x, x > p && b)
+   where
+    (p, b) = greater xs
+
+  (_, res) = greater [ f i | i <- reverse [0..n]]  
+
 
 -- Exercise 9.9 Page 160-161
+iter :: Int -> (a -> a) -> a -> a
+iter 0 _ x = x
+iter n f x = f (iter (n-1) f x)
+
+iter2 :: Int -> (a -> a) -> (a -> a)
+iter2 0 _ = (\ x -> x)
+iter2 n f = f . (iter2 (n-1) f)
+
+
 -- Exercise 9.10 Page 161
+double :: Int -> Int
+double x = 2 * x
+
+pow2 :: Int -> Int
+pow2 n = iter n double 1
+
+
 -- Exercise 9.11 Page 163
+sumSquare :: Int -> Int
+sumSquare n = foldr (+) 0 (map (\ x -> x*x) [1..n])
+
+
 -- Exercise 9.16 Page 164
+filterFirst :: (a -> Bool) -> [a] -> [a]
+filterFirst _ [] = []
+filterFirst p (x:xs)
+ | p x = x : filterFirst p xs
+ | otherwise = xs
+
+returnLoan :: (a -> Bool) -> [a] -> a
+returnLoan _ [] = error "Could not find a element in the list that have the propery"
+returnLoan p (x:xs)
+ | length (x:xs) == length filterd = x
+ | otherwise = returnLoan p filterd 
+  where
+   filterd = filterFirst p (x:xs)
+
+
 -- Exercise 9.17 Page 164
+filterLast :: (a -> Bool) -> [a] -> [a]
+filterLast p xs = reverse (filterFirst p (reverse xs))
+
+
 -- Exercise 10.3 Page 171
 -- Exercise 10.7 Page 175
 -- Exercise 10.8 Page 175
