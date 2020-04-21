efeb9cfc821a3bffd8c5df530349d87edc76b46b
Blinningjr <nicke.l@telia.com>
2020-04-21 17:34:36 +0200

    Finished some more exercises for lecture 4

--- haskell/exercises/exercises4.hs
+++ haskell/exercises/exercises4.hs
@@ -21,12 +21,68 @@ oneLookupSecond ((f,s):rest) x
 
 
 -- Exercise 12.4 Page 219
+class Visible a where
+ toString :: a -> String
+ size :: a -> Int
 
+instance Visible Bool where
+ toString True = "True"
+ toString False = "False"
+ size _ = 1
+
+instance (Visible a, Visible b) => Visible (a, b) where
+ toString (f, s) = "(" ++ toString f ++ ", " ++ toString s ++ ")"
+ size (f, s) = size f + size s
+
+instance (Visible a, Visible b, Visible c) => Visible  (a, b, c) where
+ toString (f, s, t) = "(" ++ toString f ++ ", " ++ toString s ++ ", " ++ toString t ++ ")"
+ size (f, s, t) = size f + size s + size t
 
 
 -- Exercise 12.5 Page 219
+toStringInt :: Int -> String
+toStringInt 0 = "0"
+toStringInt 1 = "1"
+toStringInt 2 = "2"
+toStringInt 3 = "3"
+toStringInt 4 = "4"
+toStringInt 5 = "5"
+toStringInt 6 = "6"
+toStringInt 7 = "7"
+toStringInt 8 = "8"
+toStringInt 9 = "9"
+toStringInt x = toStringInt (div x 10) ++ toStringInt (mod x 10)
+
+instance Visible Integer where
+ toString x = toStringInt (fromInteger x)
+ size x = 2 * (fromInteger x)
+
+
 -- Exercise 12.6 Page 219
+-- What is the type of the function
+-- compare x y = size x <= size y 
+--
+-- size :: Visible a => a -> Int
+-- (<=) :: Ord a => a -> a -> Bool  In this case  Int -> Int -> Bool
+-- compare x y :: (Visible a, Visible b) => a -> b -> Bool 
+
+
 -- Exercise 12.8 Page 219
+--instance (Ord a, Ord b) => Ord (a, b) where
+-- (<) (x, y) = y > x
+-- (<=) (x, y) = y > x || x == y
+-- (>) (x, y) = y < x
+-- (>=) (x, y) = y < x || x == y
+-- max (x, y) = if x > y then x else y
+-- min (x, y) = if x > y then y else x
+-- compare (x, y)
+--  | x < y = LT
+--  | x == y = EQ
+--  | x > y = GT
+--
+--instance Ord b => Ord [b] where
+ 
+
 -- Exercise 12.10 Page 225
 -- Exercise 13.1 Page 230
 -- Exercise 13.2 Page 237
