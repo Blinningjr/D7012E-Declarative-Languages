041922047f831d718fba602b0d6773d7629c5102
Blinningjr <nicke.l@telia.com>
2020-04-22 15:00:29 +0200

    Finished some exercisese for lecture 5

--- haskell/exercises/exercises5.hs
+++ haskell/exercises/exercises5.hs
@@ -87,28 +87,150 @@ move dx dy (PShape (x,y) s) = PShape (x + dx, y + dy) s
 -- 3. show (Add (Lit 67) (Lit (-37)))
 -- show :: Show a => a -> String
 -- show (Add (Lit 67) (Lit (-37))) :: Expr -> String
--- = "67+(-37)"
+-- = "(67+(-37))"
 
 
 -- Exercise 14.16 Page 255
 data Expr = Lit Int |
             Add Expr Expr |
-            Sub Expr Expr
+            Sub Expr Expr |
+            Mul Expr Expr |
+            Div Expr Expr 
 
 size :: Expr -> Int
-size (Lit _) = 1
+size (Lit _) = 0
 size (Add le re) = 1 + size le + size re
 size (Sub le re) = 1 + size le + size re
 
 
 -- Exercise 14.17 Page 255
+size (Mul le re) = 1 + size le + size re
+size (Div le re) = 1 + size le + size re
+
+eval :: Expr -> Int
+eval (Lit n) = n
+eval (Add le re) = eval le + eval re
+eval (Sub le re) = eval le - eval re
+eval (Mul le re) = eval le * eval re
+eval (Div le re) = div (eval le) (eval re)
+
+showE :: Expr -> String
+showE (Lit n) = show n
+showE (Add le re) = "(" ++ showE le ++ "+" ++ showE re ++ ")"
+showE (Sub le re) = "(" ++ showE le ++ "-" ++ showE re ++ ")"
+showE (Mul le re) = "(" ++ showE le ++ "*" ++ showE re ++ ")"
+showE (Div le re) = "(" ++ showE le ++ "/" ++ showE re ++ ")"
+
+
 -- Exercise 14.18 Page 255
+data Expr2 = Lit2 Int |
+             Op Ops Expr Expr
+data Ops = Add2 | Sub2 | Mul2 | Div2
+
+eval2 :: Expr2 -> Int
+eval2 (Lit2 n) = n
+eval2 (Op op le re) =
+ case op of Add2 -> lv + rv
+            Sub2 -> lv - rv
+            Mul2 -> lv * rv
+            Div2 -> div lv rv
+  where
+   lv = eval le
+   rv = eval re
+
+
 -- Exercise 14.21 Page 256
+data NTree = NilT |
+             Node Int NTree NTree
+
+getSubTrees :: NTree -> (NTree, NTree)
+getSubTrees (NilT) = error "No sub trees in leaf"
+getSubTrees (Node _ lt rt) = (lt, rt)
+
+
 -- Exercise 14.22 Page 256
+elemTree :: Int -> NTree -> Bool
+elemTree _ (NilT) = False
+elemTree n (Node v lt rt)
+ | n == v = True
+ | elemTree n lt = True 
+ | elemTree n rt = True
+ | otherwise = False
+
+
 -- Exercise 14.23 Page 256
+isNilT :: NTree -> Bool
+isNilT (NilT) = True
+isNilT _ = False
+
+setMinMax :: Int -> (Int, Int) -> (Int, Int)
+setMinMax n (x, y) = (mi, ma)
+ where
+  mi = if n < x then n else x
+  ma = if n > y then n else y
+
+setMiMa :: (Int, Int) -> (Int, Int) -> (Int, Int)
+setMiMa (x1, y1) (x2, y2) = (mi, ma)
+ where
+  mi = if x1 < x2 then x1 else x2
+  ma = if y1 > y2 then y1 else y2
+
+minMax :: NTree -> (Int, Int)
+minMax (NilT) = error "No values in leaf"
+minMax (Node v le re)
+ | isLt && isRt = (v, v)
+ | isLt = setMinMax v (minMax le)
+ | isRt = setMinMax v (minMax re)
+ | otherwise = setMiMa (minMax le) (setMinMax v (minMax re))
+  where
+   isLt = isNilT le
+   isRt = isNilT re
+
+
 -- Exercise 14.24 Page 256
+reflect :: NTree -> NTree
+reflect (NilT) = NilT
+reflect (Node v le re) = Node v (reflect re) (reflect le)
+
+
 -- Exercise 14.28 Page 260
+data Tree a = Nil | NodeT a (Tree a) (Tree a)
+              deriving (Eq, Ord, Show, Read)
+
+isLeaf :: Tree a -> Bool
+isLeaf (Nil) = True
+isLeaf _ = False
+
+elemT :: Eq a => a -> Tree a -> Bool
+elemT _ (Nil) = False
+elemT n (NodeT v lt rt)
+ | n == v = True
+ | otherwise = elemT n lt || elemT n rt
+
+
 -- Exercise 14.29 Page 
+twist :: Either a b -> Either b a
+twist (Left l) = Right l
+twist (Right r) = Left r 
+
+
 -- Exercise 14.33 Page 260
+data GTree a = Leaf a | Gnode [GTree a]
+
+countGT :: GTree a -> Int
+countGT (Leaf _) = 1
+countGT (Gnode xs) = foldr (\ x y -> y + countGT x) 0 xs
+
+depthGT :: GTree a -> Int
+depthGT (Leaf _) = 1
+depthGT (Gnode xs) = foldr (\ x y -> max (countGT x) y) 0 xs
+
+sumGT :: Num a => GTree a -> a
+sumGT (Leaf v) = v
+sumGT (Gnode xs) = foldr (\ x y -> y + sumGT x) 0 xs
+
+elemGT :: Eq a => a -> GTree a -> Bool
+
+
 -- Exercise 14.34 Page 260
 
